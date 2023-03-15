d2b69b7c2a712998a4f4ad68839d61cd56d32090
fitthuctap33 <fitthuctap33@fpts.com.vn>
2023-03-15 16:49:42 +0700

    clean code webapi

---
+++ WebAPI/WebAPI/Buyer.cs
@@ -0,0 +1,9 @@
+﻿namespace WebAPI
+{
+    public class Buyer
+    {
+        public int id{ get; set; }
+        public string name{ get; set; }
+        public string paymentmethod{ get; set; }
+    }
+}
---
+++ WebAPI/WebAPI/ProceduceModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Oracle.ManagedDataAccess.Client;
+
+namespace WebAPI
+{
+    public interface IProceduceModel
+    {
+        public bool Create(OracleConnection connection, object obj);
+        public bool Update(OracleConnection connection, object obj);
+        public bool Delete(OracleConnection connection, int id);
+        public string Get(OracleConnection connection);
+    }
+}
