ee9e5982822a966cbf937cf2b5943b71eba1137b
fitthuctap33 <fitthuctap33@fpts.com.vn>
2023-03-15 16:32:28 +0700

    clean code wwebapi

---
+++ WebAPI/ReadFile/Controllers/readController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+
+namespace ReadFile.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class readController : ControllerBase
+    {
+        private readonly string _inputFolder;
+
+        public readController(IConfiguration config)
+        {
+            _inputFolder = config.GetValue<string>("InputFolder");
+        }
+        [HttpGet]
+        [Route("read_file")]
+        public IActionResult ReadFileToObject()
+        {
+            using (StreamReader r = new StreamReader(_inputFolder))
+            {
+                string json = r.ReadToEnd();
+                return Ok(json);
+            }
+        }
+    }
+}
---
+++ WebAPI/WebAPI/BuyerProceduce.cs
@@ -0,0 +1,86 @@
+﻿using Newtonsoft.Json;
+using Oracle.ManagedDataAccess.Client;
+using Oracle.ManagedDataAccess.Types;
+using System.Data;
+
+namespace WebAPI;
+
+public class BuyerProceduce : IProceduceModel
+{
+    public bool Create(OracleConnection connection, Buyer buyer)
+    {
+        try
+        {
+            connection.Open();
+            var command = new OracleCommand("addBuyer", connection);
+            command.CommandType = CommandType.StoredProcedure;
+            command.Parameters.Add("id", OracleDbType.Int32, ParameterDirection.Input).Value = buyer.id;
+            command.Parameters.Add("name", OracleDbType.Varchar2, ParameterDirection.Input).Value = buyer.name;
+            command.Parameters.Add("paymentMethod", OracleDbType.Varchar2, ParameterDirection.Input).Value = buyer.paymentmethod;
+            command.ExecuteNonQuery();
+            connection.Close();
+            return true;
+        }
+        catch (Exception ex)
+        {
+            return false;
+        }
+    }
+
+    public bool Update(OracleConnection connection, Buyer buyer)
+    {
+        connection.Open();
+        var command = new OracleCommand("updateBuyer", connection);
+        command.CommandType = CommandType.StoredProcedure;
+        command.Parameters.Add("id", OracleDbType.Int32, ParameterDirection.Input).Value = buyer.id;
+        command.Parameters.Add("name", OracleDbType.Varchar2, ParameterDirection.Input).Value = buyer.name;
+        command.Parameters.Add("paymentMethod", OracleDbType.Varchar2, ParameterDirection.Input).Value = buyer.paymentmethod;
+        command.Parameters.Add("success", OracleDbType.Int32, ParameterDirection.Output);
+        // Thực thi stored procedure
+        command.ExecuteNonQuery();
+        // Lấy giá trị trả về từ output parameter
+        int success = ((OracleDecimal)command.Parameters["success"].Value).ToInt32();
+        connection.Close();
+        if (success == 0)
+        {
+            return false;
+        }
+        return true;
+    }
+    public bool Delete(OracleConnection connection, int id)
+    {
+        connection.Open();
+        var command         = new OracleCommand("deleteBuyer", connection);
+        command.CommandType = CommandType.StoredProcedure;
+        command.Parameters.Add("id", OracleDbType.Varchar2, ParameterDirection.Input).Value = id;
+        command.Parameters.Add("out_success", OracleDbType.Int32, ParameterDirection.Output);
+        command.ExecuteNonQuery();
+
+        int success = ((OracleDecimal)command.Parameters["out_success"].Value).ToInt32();
+        connection.Close();
+        return success == 1 ? true : false;
+    }
+    public string Get(OracleConnection connection)
+    {
+        connection.Open();
+        var command = new OracleCommand("getBuyers", connection);
+        command.CommandType = CommandType.StoredProcedure;
+        command.Parameters.Add("result", OracleDbType.RefCursor, ParameterDirection.Output);
+        var reader = command.ExecuteReader();
+        var dataTable = new DataTable();
+        dataTable.Load(reader);
+        var json = JsonConvert.SerializeObject(dataTable, Formatting.Indented);
+        connection.Close();
+        return json;
+    }
+
+    public bool Create(OracleConnection connection, object obj)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Update(OracleConnection connection, object obj)
+    {
+        throw new NotImplementedException();
+    }
+}
