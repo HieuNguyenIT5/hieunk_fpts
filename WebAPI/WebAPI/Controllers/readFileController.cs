f0fc99f8dfed2ad34788b5a58d8d616210aaf49a
fitthuctap33 <fitthuctap33@fpts.com.vn>
2023-03-15 16:23:07 +0700

    clean code webapi

---
+++ WebAPI/WebAPI/Controllers/readFileController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class readFileController : ControllerBase
+    {
+        private readonly string _inputFolder;
+
+        public readFileController(IConfiguration config)
+        {
+            _inputFolder = config.GetValue<string>("InputFolder");
+        }
+        [HttpGet]
+        [Route("read_file")]
+        public IActionResult ReadFileToObject()
+        {
+            using (StreamReader r = new StreamReader(_inputFolder))
+            {
+                string json = r.ReadToEnd();
+                return Ok(json);
+            }
+        }
+    }
+}
